be9a3aac5f061116aa64d3107ac4b8eaf6bc0614
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-03-30 14:55:07 +0530

    Highlight selected answers

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -3,19 +3,44 @@ import QUESTIONS from '../utils/questions';
 import Summary from './Summary';
 import QuestionTimer from './QuestionTimer';
 
+interface Answer {
+  answer: 'answered' | 'unanswered' | 'correct' | 'wrong';
+}
+
 function Quiz() {
+  const [answerState, setAnswerState] = useState<Answer>({
+    answer: 'unanswered',
+  });
   const [userAnswers, setUserAnswers] = useState<string[]>([]);
 
-  const activeQuestionIndex = userAnswers.length;
+  const activeQuestionIndex =
+    answerState.answer === 'unanswered'
+      ? userAnswers.length
+      : userAnswers.length - 1;
 
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-  const handleSelectAnswer = useCallback((selectedAnswer: string) => {
-    setUserAnswers((prevAnswers) => {
-      return [...prevAnswers, selectedAnswer];
-    });
-  }, []);
+  const handleSelectAnswer = useCallback(
+    (selectedAnswer: string) => {
+      setAnswerState({ answer: 'answered' });
+      setUserAnswers((prevAnswers) => {
+        return [...prevAnswers, selectedAnswer];
+      });
 
+      setTimeout(() => {
+        if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
+          setAnswerState({ answer: 'correct' });
+        } else {
+          setAnswerState({ answer: 'wrong' });
+        }
+
+        setTimeout(() => {
+          setAnswerState({ answer: 'unanswered' });
+        }, 2000);
+      }, 1000);
+    },
+    [activeQuestionIndex]
+  );
   // If the timer expired a question is skipped
   const handleSkipAnswer = useCallback(() => {
     handleSelectAnswer('');
@@ -34,15 +59,36 @@ function Quiz() {
         <QuestionTimer
           key={activeQuestionIndex}
           timeout={10000}
-          onTimeout={() => handleSelectAnswer('')}
+          onTimeout={handleSkipAnswer}
         />
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id='answers'>
-          {shuffledAnswers.map((answer) => (
-            <li key={answer} className='answer'>
-              <button onClick={handleSkipAnswer}>{answer}</button>
-            </li>
-          ))}
+          {shuffledAnswers.map((answer) => {
+            const isSelected = userAnswers[userAnswers.length - 1] === answer;
+            let answerClass = ``;
+
+            if (answerState.answer === 'answered' && isSelected) {
+              answerClass = `selected`;
+            }
+
+            if (
+              (answerState.answer === 'correct' ||
+                answerState.answer === 'wrong') &&
+              isSelected
+            ) {
+              answerClass = answerState.answer;
+            }
+            return (
+              <li key={answer} className='answer'>
+                <button
+                  onClick={() => handleSelectAnswer(answer)}
+                  className={answerClass}
+                >
+                  {answer}
+                </button>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
